c7558e4cd44e2374edaac7035e6e0c76962d298a
vagalep <ivagalep@gmail.com>
2016-05-05 17:27:48 +0300

    Pudge(fix1)

--- src/Pudge/Fight.java
+++ src/Pudge/Fight.java
@@ -5,31 +5,39 @@ public class Fight {
 	public static void main(String[] args) {
 		boolean end = false;
 		Pudge Pudge1 = new Pudge(5, 5, 5, "Bichina");
-		Pudge Pudge2 = new Pudge(5, 1, 5, "Rvanina");
-
+		Pudge Pudge2 = new Pudge(5, 5, 5, "Rvanina");
+		Pudge1.getInfo();
+		Pudge1.getInfo();
+		System.out.println();
 		while (!end) {
 			if (Pudge1.setHelth() > 0 && Pudge2.setHelth() > 0) {
-				Pudge2.getHelth(Pudge1.setStrike());
-				if (Pudge2.setHelth() > 0) {
-					Pudge2.getInfo();
-				} else {
-					System.out.print(Pudge1.getObjName()+" Lose        ");
-				}
 				Pudge1.getHelth(Pudge2.setStrike());
 				if (Pudge1.setHelth() > 0) {
 					Pudge1.getInfo();
 				} else {
+					System.out.print(Pudge1.getObjName()+" Lose        ");
+					end = true;
+					break;
+				}
+				Pudge2.getHelth(Pudge1.setStrike());
+				if (Pudge2.setHelth() > 0) {
+					Pudge2.getInfo();
+				} else {
 					System.out.println(Pudge2.getObjName()+" Lose        ");
+					end = true;
+					break;
 				}
 			} else {
 				end = true;
 			}
 			System.out.println();
 		}
+		System.out.println();
+		System.out.println();
 		if (Pudge1.setHelth() >= 0) {
-			System.out.print("Resolution: "+Pudge1.getObjName()+" Win");
+			System.out.print("             Resolution: "+Pudge1.getObjName()+" Win");
 		} else {
-			System.out.print("    Resolution: "+Pudge2.getObjName()+" Win");
+			System.out.print("             Resolution: "+Pudge2.getObjName()+" Win");
 		}
 	}
 }
